f713c35432f13cf4182b2c10d784810133cef967
RatInTheCave <arslan.saadat@auk.edu.ua>
2023-08-23 12:25:21 +0300

    Saadat-Arslan-Project-1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.print("Hello world!");
+        System.out.println(" My name is Saadat Arslan. Welcome to my Java program");
+        System.out.println("e");
+        System.out.println("l");
+        System.out.println("l");
+        System.out.println("o");
+        System.out.println(" ");
+        System.out.println("w");
+        System.out.println("o");
+        System.out.println("r");
+        System.out.println("l");
+        System.out.println("d");
+        System.out.println("!");
+
+    }
+}
